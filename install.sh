2a829bb910a23435cd15a7685fa4cc9f7d8d8980
Yevgeny Kuznetsov <yevgenyku@gmail.com>
2020-07-13 01:32:43 +0300

    Add apt upgrade stage before package installation

--- install.sh
+++ install.sh
@@ -52,6 +52,7 @@ function install_packages() {
 
   sudo apt-get update
   sudo bash -c 'DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::options::=--force-confdef -o DPkg::options::=--force-confold upgrade -y'
+  sudo apt-get upgrade -y
   sudo apt-get install -y "${packages[@]}" || echo -e " Try cleaning dpkg cache:\n\
   sudo dpkg -i --force-overwrite PROBLEMATIC_PACKAGE_FROM_ERROR_MESSAGE\n\
   To fix broken packages run:\n\
