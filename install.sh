9b0bc035667b74c925dc9d6da2885c7c3578e3de
Yevgeny Kuznetsov <yevgenyku@gmail.com>
2020-11-19 03:00:58 +0200

    Bump go version

--- install.sh
+++ install.sh
@@ -266,7 +266,7 @@ function install_java() {
 
 # Install go:
 function install_go() {
-  local v=1.15.2
+  local v=1.15.5
   ! command -v go &>/dev/null || [[ "$(go version)" != *"$v"* ]] || return 0
   echo "-----> Install Golang $v"
   dwfile="go${v}.linux-amd64.tar.gz"
