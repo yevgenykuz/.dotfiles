91016ba5dab4c1150f0933b703717e055541cf62
Yevgeny Kuznetsov <yevgenyku@gmail.com>
2021-02-23 22:12:26 -0500

    Bump JDK versions

--- sdkman.sh
+++ sdkman.sh
@@ -6,9 +6,9 @@ curl -s "https://get.sdkman.io?rcupdate=false" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh" || sdk version || echo "Try running \"source \
 \"$HOME/.sdkman/bin/sdkman-init.sh\"\""
 # Install JDKs
-yes | sdk install java 8.0.275.hs-adpt || true
-yes | sdk install java 11.0.9.hs-adpt || true
-sdk default java 11.0.9.hs-adpt || true
+yes | sdk install java 8.0.282.hs-adpt || true
+yes | sdk install java 11.0.10.hs-adpt || true
+sdk default java 11.0.10.hs-adpt || true
 # jdks can be found at: ~/.sdkman/candidates/java
 # Install maven and gradle
 yes | sdk install maven || true
