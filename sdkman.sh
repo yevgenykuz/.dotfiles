f475cc1155cdb4acfc08a42c32bab1be9f2e3481
Yevgeny Kuznetsov <yevgenyku@gmail.com>
2022-07-19 01:30:14 -0400

    Update Java versions

--- sdkman.sh
+++ sdkman.sh
@@ -6,10 +6,10 @@ curl -s "https://get.sdkman.io?rcupdate=false" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh" || sdk version || echo "Try running \"source \
 \"$HOME/.sdkman/bin/sdkman-init.sh\"\""
 # Install JDKs
-yes | sdk install java 8.0.302-tem || true
-yes | sdk install java 11.0.12-tem || true
-yes | sdk install java 17.0.0-tem || true
-sdk default java 11.0.12-tem || true
+yes | sdk install java 8.0.332-tem || true
+yes | sdk install java 11.0.15-tem || true
+yes | sdk install java 18.0.1-tem || true
+sdk default java 11.0.15-tem || true
 # jdks can be found at: ~/.sdkman/candidates/java
 # Install maven and gradle
 yes | sdk install maven || true
